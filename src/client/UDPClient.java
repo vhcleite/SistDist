b7c9e5d741a4346e9def51d742a40118c3acda28
Victor Leite <victor_leite@outlook.com.br>
2019-06-13 01:01:07 -0300

    EP0 - Correção de condição para finalização do método main

--- src/client/UDPClient.java
+++ src/client/UDPClient.java
@@ -7,12 +7,13 @@ class UDPClient {
     ClientThread client1 = new ClientThread(TestCaseEnum.ORDELY_MESSAGES);
     ClientThread client2 = new ClientThread(TestCaseEnum.LOST_MESSAGES);
     
-    System.out.println("Inicializando cliente");
+    System.out.println("Start do cliente1");
     client1.start();
+    System.out.println("Start do cliente2");
     client2.start();
     System.out.println("Clientes inicializados");
     
-    while (client1.isAlive() && client2.isAlive()) {
+    while (client1.isAlive() || client2.isAlive()) {
       try {
         Thread.sleep(300);
       } catch (InterruptedException e) {
